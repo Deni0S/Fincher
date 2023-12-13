17f276e95caa6d0cd6bed7d1cf9d1d3acfc00835
androidc <chizztectep@gmail.com>
2023-12-13 16:59:41 +0300

    bug fix

--- Fincher/Core/Extension/MainViewController+Ext.swift
+++ Fincher/Core/Extension/MainViewController+Ext.swift
@@ -5,11 +5,8 @@ import UIKit
 
 extension MainViewController: UITextViewDelegate {
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        // Compute the value of the text view after the text is inserted.
         let currentText = textView.text ?? ""
-        // attempt to read the range they are trying to change, or exit if we can't
         guard let stringRange = Range(range, in: currentText) else { return false }
-        // add their new text to the existing text
         let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
         switch textView.tag {
         case 1:
--- FincherTests/FincherTests.swift
+++ FincherTests/FincherTests.swift
@@ -6,60 +6,42 @@ final class FincherTests: XCTestCase {
     let viewModel = MainViewModel()
     var paymentsCalendar: [Payments] = []
     
-    override func setUpWithError() throws {
-        
-    }
+    override func setUpWithError() throws {}
     
-    override func tearDownWithError() throws {
-        
-    }
+    override func tearDownWithError() throws {}
     
     func testCalculateAnnuitentPayments() throws {
-        // given
         viewModel.amountOfCredit = 1000
         viewModel.interestRate = 10
         viewModel.creditTerm = 1
-        // when
-        let result = viewModel.calculateAnnuitentPayments()
-        // then
+        let result = try! viewModel.calculateAnnuitentPayments()
         XCTAssertEqual(String(format: "%.2f", result),"54.99")
     }
     
     func testCalculateDiffPayments() throws {
-        //given
         viewModel.amountOfCredit = 1000
         viewModel.interestRate = 10
         viewModel.creditTerm = 1
-        //when
         var result: (Double, Double, Double) = (0, 0, 0)
-        result = viewModel.calculateDiffPayments()
-        //Then
+        result = try! viewModel.calculateDiffPayments()
         XCTAssertEqual(String(format: "%.2f", result.2),"54.17")
     }
     
     func testCalculateTermPayments() throws {
-        //given
         viewModel.amountOfCredit = 1000
         viewModel.interestRate = 10
         viewModel.paymentSetted = 100
-        //when
         var result: (Int, Double) = (0, 0)
-        result = viewModel.calculateTermPayments()
-        // Then
+        result = try! viewModel.calculateTermPayments()
         XCTAssertEqual(String(format: "%.2f", result.1),"48.58")
-        
     }
     
     func testCalculateMaxCredit() throws {
-        // given
         viewModel.interestRate = 10
         viewModel.creditTerm = 1
         viewModel.paymentSetted = 1000
-        // when
         var result: (Double, Double) = (0, 0)
-        result = viewModel.calculateMaxCredit()
-        //then
-        // Then
+        result = try! viewModel.calculateMaxCredit()
         XCTAssertEqual(String(format: "%.2f", result.1),"625.49")
     }
 }
